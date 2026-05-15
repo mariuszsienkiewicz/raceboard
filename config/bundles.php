fee55c314061a9d14ef03ecd707b867f473090be
Mariusz Sienkiewicz <mariusz.sienkiewicz.it@gmail.com>
2026-05-15 17:49:08 +0200

    feat(auth): add JWT authentication with login endpoint (#11)

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
